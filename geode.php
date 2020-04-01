12aee95f8ab5fcb3f47883f587182482de468137
ShemarSinclairennis <shemar.ennis@gmail.com>
2020-04-01 09:47:08 -0500

    Files added

---
+++ geode.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Geode</title>
+	<link rel="stylesheet" type="text/css" href="styles/geode.css">
+</head>
+<body>
+	<body>
+
+<!--Navbar-->
+	<ul>
+	  <li><a href="">Home</a></li>
+	  <li><a href="">The Geode</a></li>
+	  <li><a href="">Awards</a></li>
+	  <li><a href="">Caribbean Journal of Earth</a></li>
+	  <li><a href="">Professional Geologist</a></li>
+	  <li><a href="">About The GSJ</a></li>
+	  <li><a href="">Events</a></li>
+	  <li><a href="">Current Council</a></li>
+	  <li><a href="">Membership</a></li>
+
+	</ul>
+
+
+<!--Grid layout for page -->
+<div class="grid-container">
+  <div class="Infor">Info</div>
+  <div class="Header"><br> &nbsp&nbsp&nbsp&nbsp The Geode</div>
+  <div class="Footer1"> <br><img src="images/logo1.png" ></div>
+  <div class="Footer2"><br>Contact:<br>Geological Society of Jamaica,<br>C/O Department of Geography and Geology,<br>University of the West Indies,<br>Mona, Kingston 7, Jamaica.</div>
+  <div class="Footer3"><br>Email:<br>info@geologicalsocietyofJamaica.org</div>
+  <div class="space">-</div>
+</div>
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Geological Society of Jamaica</title>
+	<link rel="stylesheet" type="text/css" href="styles/home.css">
+</head>
+<body>
+	<!--Navbar-->
+	<ul>
+	  <li><a href="">Home</a></li>
+	  <li><a href="">The Geode</a></li>
+	  <li><a href="">Awards</a></li>
+	</ul>
+
+	
+
+<div class="grid-container">
+	<!--SlideShow still need work-->
+  <div class="slideshow"><div id="stage">
+	<a href="images/geo1.jpg"><img src="images/geo1.jpg" width="1124" height="542"></a>
+	<a href="images/geo2.jpg"><img src="images/geo2.jpg" width="1124" height="542"></a>
+	<a href="images/geo3.jpg"><img src="images/geo3.jpg" width="1124" height="542"></a>
+	</div> </div>
+
+
+	<!--Grid layout for tiles-->
+  <div class="footer">3</div>
+  <div class="ta"><a>CARIBBEAN JOURNAL OF EARTH</a></div>
+  <div class="tb "><br>PROFESSIONAL GEOLOGISTS</div>
+  <div class="tc "><br>ABOUT THE GSJ</div>
+  <div class="td "><br>EVENTS</div>
+  <div class="te "><br>CURRENT COUNCIL</div>
+  <div class="tf "><br>MEMBERSHIP</div>
+</div>
+
+
+<script src="javascript/home.js"></script>
+</body>
+</html>
